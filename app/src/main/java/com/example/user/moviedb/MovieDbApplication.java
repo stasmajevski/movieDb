a69e1c3547cfc144e61f7bd343aa18dc3a9c2a8b
stasmajevski <stas@tlu.ee>
2018-04-15 14:16:15 +0300

    added custom class, changed default colors
    only portrait mode in all activities

---
+++ app/src/main/java/com/example/user/moviedb/MovieDbApplication.java
@@ -0,0 +1,49 @@
+package com.example.user.moviedb;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class MovieDbApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle bundle) {
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+}
